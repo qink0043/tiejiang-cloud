13a3f122bd7078ad45bff959da8e2823e243f77c
qink0043 <3208013769@qq.com>
2025-09-07 00:05:30 +0800

    配置别名，创建路由文件

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.scss'
+import {RouterProvider} from 'react-router-dom'
+import router from './routes'
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!)
+root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
