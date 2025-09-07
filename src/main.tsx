b4a05e2649d1679814baa4d663a56e75151f7b1a
qink0043 <3208013769@qq.com>
2025-09-07 17:02:57 +0800

    修复anmtd不兼容react19问题

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ConfigProvider, theme } from 'antd';
-
-import './styles/theme.scss'; // 导入全局 SCSS 文件
+import 'antd/dist/reset.css'
+import '@ant-design/v5-patch-for-react-19';
+import './styles/theme.scss';
 import router from './routes';
 import { ThemeProvider, ThemeContext } from './contexts/ThemeContext';
 
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -8,16 +8,15 @@ const Login: React.FC = () => {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   return (
     <div className="login">
-      <Switch checked={isDarkMode} onChange={toggleTheme} />
-
+      <Switch checked={isDarkMode} onChange={toggleTheme} className='theme-switch'/>
       <Card className="login-container">
         <img src={logo} alt="logo" className="login-logo" />
         {/* 登录表单 */}
         <Form>
-          <Form.Item>
+          <Form.Item name="username">
             <Input size='large' placeholder="请输入用户名或邮箱" />
           </Form.Item>
-          <Form.Item>
+          <Form.Item name="password">
             <Input size='large' placeholder="请输入密码" />
           </Form.Item>
           <Form.Item>
